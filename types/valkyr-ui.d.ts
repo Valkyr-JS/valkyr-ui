796f0145acb9e088fb6cea105945aedc1af33163
ValkyrJS <valkyr.photos@gmail.com>
2026-01-05 12:58:09 +0000

    Removed demo types

--- types/valkyr-ui.d.ts
+++
@@ -1,5 +0,0 @@
-/** Types for your plugin config. Settings are undefined by default. Data should
- * match the settings in your `/src/source.yml` file. */
-interface MyPluginConfig {
-  enablePlugin?: boolean;
-}
