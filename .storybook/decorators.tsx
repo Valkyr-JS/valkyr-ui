cc8afcf412ab96d4c0716d935f94bc2d2c4f5d81
ValkyrJS <valkyr.photos@gmail.com>
2026-01-05 18:39:08 +0000

    React Intl for Storybook, with Stash default locale

---
+++ .storybook/decorators.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { IntlProvider } from "react-intl";
+import { DecoratorFunction } from "storybook/internal/csf";
+import messages from "./locales/en-GB.json";
+import { ReactRenderer } from "@storybook/react-vite";
+
+type NestedMessage = { [key: string]: NestedMessage | string };
+/** https://github.com/stashapp/stash/blob/develop/ui/v2.5/src/utils/flattenMessages.ts */
+const flattenMessages = (
+  nestedMessages: NestedMessage | null,
+  prefix = ""
+): Record<string, string> => {
+  if (nestedMessages === null) {
+    return {};
+  }
+  return Object.keys(nestedMessages).reduce((messages, key) => {
+    const value = nestedMessages[key];
+    const prefixedKey = prefix ? `${prefix}.${key}` : key;
+    const flatRecord =
+      typeof value === "string"
+        ? { [prefixedKey]: value }
+        : flattenMessages(value, prefixedKey);
+
+    Object.assign(messages, flatRecord);
+
+    return messages;
+  }, {});
+};
+
+/** Wraps a story in react-intl's `IntlProvider` component and provides access
+ * to Stash's `en-GB` locale messages. */
+export const WithIntlProvider: DecoratorFunction<ReactRenderer> = (Story) => (
+  <IntlProvider locale="en-GB" messages={flattenMessages(messages)}>
+    <Story />
+  </IntlProvider>
+);
