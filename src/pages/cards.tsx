3aac46b6eef9913e8ba509d506243f32cc97d138
ValkyrJS <valkyr.photos@gmail.com>
2026-01-06 14:07:52 +0000

    vui-scene-cards-enabled setting

--- src/pages/cards.tsx
+++ src/pages/cards.tsx
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form } from "react-bootstrap";
+import { BooleanSetting } from "@/components/stash/Settings/Inputs";
 import { SettingSection } from "@/components/stash/Settings/SettingSection";
 
-const CardsTab: React.FC = () => {
+interface CardSettings {
+  sceneCardsEnabled?: boolean;
+}
+
+interface CardsTabProps {
+  configUpdateHandler: (updatedSettings: CardSettings) => void;
+  cardSettings: CardSettings;
+}
+
+const CardsTab: React.FC<CardsTabProps> = (props) => {
+  const [cardSettings, setCardSettings] = useState(props.cardSettings);
+
   return (
     <Form.Group>
       <SettingSection id="cards-enable" heading="Enable cards">
-        Hello
+        <BooleanSetting
+          checked={cardSettings.sceneCardsEnabled ?? false}
+          heading="Enable scene cards"
+          id="vui-scene-cards-enabled"
+          onChange={() =>
+            setCardSettings({
+              ...cardSettings,
+              sceneCardsEnabled: !cardSettings.sceneCardsEnabled,
+            })
+          }
+        />
       </SettingSection>
     </Form.Group>
   );
