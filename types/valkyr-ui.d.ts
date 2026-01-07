126d6af0dd4a01c83caaecbe880db77e8a3ee454
Valkyr-JS <154020147+Valkyr-JS@users.noreply.github.com>
2026-01-07 13:37:03 +0000

    Plugin config option for card grids (#4)
    * Only enable scene card grid if both options are enabled
    
    * Config option

--- types/valkyr-ui.d.ts
+++ types/valkyr-ui.d.ts
@@ -9,6 +9,8 @@ type StashCardGridZoom = 0 | 1 | 2 | 3;
 /** The config for the Valkyr UI plugin. Stash only creates config properties
  * when they are changed. By default they are `undefined`. */
 interface ValkyrUiConfigMap {
-  /** Enables Valkyr scene cards */
+  /** Enables Valkyr UI card grids for all enabled card types. */
+  cards__cardGrids__enabled?: boolean;
+  /** Enables Valkyr UI scene cards. */
   cards__sceneCards__enabled?: boolean;
 }
