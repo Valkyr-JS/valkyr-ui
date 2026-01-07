d55b58dd421fc3c3b6d6a0df30c41153e25903db
ValkyrJS <valkyr.photos@gmail.com>
2026-01-07 01:17:21 +0000

    WithCardGrid story

--- src/components/cards/SceneCard/SceneCard.stories.tsx
+++ src/components/cards/SceneCard/SceneCard.stories.tsx
@@ -1,7 +1,9 @@
+import React from "react";
 import type { Meta, StoryObj } from "@storybook/react-vite";
 import { SceneCard } from ".";
 import scene2414 from "../../../../mocks/scenes/2414.json";
 import scene10613 from "../../../../mocks/scenes/10613.json";
+import CardGrid from "../CardGrid";
 
 const meta = {
   title: "Modules/Cards/SceneCard",
@@ -33,4 +35,17 @@ export const Scene10613: Story = {
   args: {
     scene: scene10613 as SlimSceneDataFragment,
   },
+};
+
+export const WithCardGrid: Story = {
+  args: {
+    scene: scene2414 as SlimSceneDataFragment,
+  },
+  decorators: [
+    (Story) => (
+      <CardGrid>
+        <Story />
+      </CardGrid>
+    ),
+  ],
 };
