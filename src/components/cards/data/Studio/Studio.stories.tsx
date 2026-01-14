51a3642d3beaeecc998fe65cef969e72e0ab0b53
ValkyrJS <valkyr.photos@gmail.com>
2026-01-14 17:26:53 +0000

    Remopved WithCard decorator from studio stories

--- src/components/cards/data/Studio/Studio.stories.tsx
+++ src/components/cards/data/Studio/Studio.stories.tsx
@@ -1,7 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
 import { expect, within } from "storybook/test";
 import Studio from ".";
-import { WithCard } from "../../../../../.storybook/decorators";
 
 const meta = {
   title: "Components/Cards/Data/Studio",
