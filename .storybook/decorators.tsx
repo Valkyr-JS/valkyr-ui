7099bf297db9fcb91f2e8fcc31032ba1a87e0c75
ValkyrJS <valkyr.photos@gmail.com>
2026-01-14 17:04:52 +0000

    WithCard decorator

--- .storybook/decorators.tsx
+++ .storybook/decorators.tsx
@@ -3,6 +3,7 @@ import { IntlProvider } from "react-intl";
 import { DecoratorFunction } from "storybook/internal/csf";
 import messages from "./locales/en-GB.json";
 import { ReactRenderer } from "@storybook/react-vite";
+import { Card } from "react-bootstrap";
 
 type NestedMessage = { [key: string]: NestedMessage | string };
 /** https://github.com/stashapp/stash/blob/develop/ui/v2.5/src/utils/flattenMessages.ts */
@@ -33,4 +34,11 @@ export const WithIntlProvider: DecoratorFunction<ReactRenderer> = (Story) => (
   <IntlProvider locale="en-GB" messages={flattenMessages(messages)}>
     <Story />
   </IntlProvider>
+);
+
+/** Wraps a story in simple React Bootstrap card component. */
+export const WithCard: DecoratorFunction<ReactRenderer> = (Story) => (
+  <Card>
+    <Story />
+  </Card>
 );
