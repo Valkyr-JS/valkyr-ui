569ea45e47fd7bba41f2aff2238277d501e8d7e6
ValkyrJS <valkyr.photos@gmail.com>
2026-01-15 23:46:59 +0000

    Form class names formatting

--- src/pages/cards/index.tsx
+++ src/pages/cards/index.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { Form } from "react-bootstrap";
-import { CLASSNAME } from "@/constants";
 import EnableCardsSection from "./EnableCardsSection";
 import SharedCardDataSection from "./SharedCardDataSection";
 
 const CardsTab: React.FC<SettingsTabProps> = (props) => {
-  const classes = CLASSNAME.NAMESPACE + "__form-group";
+  const componentClassname = "vui-form-group";
 
   return (
-    <Form.Group className={classes}>
+    <Form.Group className={componentClassname}>
       <EnableCardsSection {...props} />
       <SharedCardDataSection {...props} />
     </Form.Group>
