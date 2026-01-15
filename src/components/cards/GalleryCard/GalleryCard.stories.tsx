2c6290427244f849ad7e038ec3d92566f31dd66c
ValkyrJS <valkyr.photos@gmail.com>
2026-01-15 19:57:11 +0000

    GalleryCard with footer stories

--- src/components/cards/GalleryCard/GalleryCard.stories.tsx
+++ src/components/cards/GalleryCard/GalleryCard.stories.tsx
@@ -1,15 +1,31 @@
+import React from "react";
 import type { Meta, StoryObj } from "@storybook/react-vite";
 import GalleryCard from ".";
 import gallery4521 from "../../../../mocks/galleries/4521.json";
+import gallery17791 from "../../../../mocks/galleries/17791.json";
+import { fn } from "storybook/test";
 
 const meta = {
   title: "Components/Cards/Gallery card",
   component: GalleryCard,
+  decorators: [
+    (Story) => (
+      <div style={{ maxWidth: 640 }}>
+        <Story />
+      </div>
+    ),
+  ],
   parameters: {
     layout: "centered",
   },
   args: {
     pluginConfig: {},
+    zoomBreakpoint: 1,
+  },
+  argTypes: {
+    zoomBreakpoint: {
+      control: { type: "range", min: 0, max: 3 },
+    },
   },
   tags: ["autodocs"],
 } satisfies Meta<typeof GalleryCard>;
@@ -17,8 +33,33 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Gallery4521: Story = {
+const footerProps = {
+  openHandler: fn(),
+  setSection: fn(),
+};
+
+export const LandscapeThumbnail: Story = {
+  args: {
+    gallery: gallery17791 as SlimGalleryDataFragment,
+  },
+};
+
+export const LandscapeThumbnailWithFooter: Story = {
+  args: {
+    gallery: gallery17791 as SlimGalleryDataFragment,
+    footer: footerProps,
+  },
+};
+
+export const PortraitThumbnail: Story = {
   args: {
     gallery: gallery4521 as SlimGalleryDataFragment,
+  },
+};
+
+export const PortraitThumbnailWithFooter: Story = {
+  args: {
+    gallery: gallery4521 as SlimGalleryDataFragment,
+    footer: footerProps,
   },
 };
