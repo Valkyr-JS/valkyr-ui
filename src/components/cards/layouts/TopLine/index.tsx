fa6a8ae977bddfb397c28b9f492ef319287b7895
ValkyrJS <valkyr.photos@gmail.com>
2026-01-15 23:34:53 +0000

    Top Line class name formatting

--- src/components/cards/layouts/TopLine/index.tsx
+++ src/components/cards/layouts/TopLine/index.tsx
@@ -1,11 +1,10 @@
 import React, { PropsWithChildren } from "react";
-import { CLASSNAME } from "@/constants";
 import "./TopLine.scss";
 
 const TopLine: React.FC<PropsWithChildren> = (props) => {
   if (!props.children) return null;
 
-  const componentClass = CLASSNAME.NAMESPACE + "__card-top-line";
+  const componentClass = "vui-card-top-line";
   return <div className={componentClass}>{props.children}</div>;
 };
 
