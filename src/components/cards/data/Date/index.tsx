0e52e93d24459116132ae531339660bfb5e1ef2d
ValkyrJS <valkyr.photos@gmail.com>
2026-01-16 11:58:35 +0000

    Date styling

--- src/components/cards/data/Date/index.tsx
+++ src/components/cards/data/Date/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { getRenderData } from "@/helpers";
+import "./Date.scss";
 
 interface DateProps {
   /** The date data. */
-  date: Maybe<Scalars["String"]["output"]>;
+  date: Maybe<Scalars["String"]["output"]> | undefined;
 }
 
 const Date: React.FC<
