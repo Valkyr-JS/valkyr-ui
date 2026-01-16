1d00c8ca1733171395fc7fec1b1a1fa030b882a6
ValkyrJS <valkyr.photos@gmail.com>
2026-01-16 13:05:37 +0000

    Format date for partial dates

--- src/components/cards/data/Date/index.tsx
+++ src/components/cards/data/Date/index.tsx
@@ -26,6 +26,45 @@ const DateComponent: React.FC<
 
   const componentClass = "vui-card-data__date";
 
+  /* ------------------------------------- Only year available ------------------------------------ */
+
+  const yearMatch = data.match(/^(\d{4})$/);
+  if (yearMatch) {
+    const year = parseInt(yearMatch[1], 10);
+    return (
+      <span className={componentClass}>
+        <span className="sr-only">Date: </span>
+        <FormattedDate
+          value={Date.UTC(year, 0)}
+          year="numeric"
+          timeZone="utc"
+        />
+      </span>
+    );
+  }
+
+  /* -------------------------------- Only month and year available ------------------------------- */
+
+  const yearMonthMatch = data.match(/^(\d{4})-(\d{2})$/);
+  if (yearMonthMatch) {
+    const year = parseInt(yearMonthMatch[1], 10);
+    const month = parseInt(yearMonthMatch[2], 10) - 1;
+
+    return (
+      <span className={componentClass}>
+        <span className="sr-only">Date: </span>
+        <FormattedDate
+          value={Date.UTC(year, month)}
+          year="numeric"
+          month="numeric"
+          timeZone="utc"
+        />
+      </span>
+    );
+  }
+
+  /* ------------------------------------- Full date available ------------------------------------ */
+
   return (
     <span className={componentClass}>
       <span className="sr-only">Date: </span>
