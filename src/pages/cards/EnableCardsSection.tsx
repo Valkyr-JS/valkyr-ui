8a7023dfbf8843e2dcad7bc8c43857956e290ae4
ValkyrJS <valkyr.photos@gmail.com>
2026-01-16 01:37:00 +0000

    Fixed initial click not working for enable settings

--- src/pages/cards/EnableCardsSection.tsx
+++ src/pages/cards/EnableCardsSection.tsx
@@ -1,63 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 import { BooleanSetting } from "@/components/stash/Settings/Inputs";
 import { SettingSection } from "@/components/stash/Settings/SettingSection";
 import { DEFAULT } from "@/constants";
 
 const EnableCardsSection: React.FC<SettingsTabProps> = (props) => {
-  const GalleryCardsEnabled = () => (
-    <BooleanSetting
-      checked={
-        props.pluginConfig.cards__galleryCard__enabled ??
+  const GalleryCardsEnabled = () => {
+    const [checked, setChecked] = useState(
+      props.pluginConfig.cards__galleryCard__enabled ??
         DEFAULT.CARDS.GALLERY_CARD.ENABLED
-      }
-      heading="Enable gallery cards"
-      id="valkyr-ui-cards__galleryCard__enabled"
-      onChange={() =>
-        props.configUpdateHandler({
-          ...props.pluginConfig,
-          cards__galleryCard__enabled:
-            !props.pluginConfig.cards__galleryCard__enabled,
-        })
-      }
-    />
-  );
+    );
+    return (
+      <BooleanSetting
+        checked={checked}
+        heading="Enable gallery cards"
+        id="valkyr-ui-cards__galleryCard__enabled"
+        onChange={() => {
+          const newState = !checked;
+          setChecked(newState);
+          props.configUpdateHandler({
+            ...props.pluginConfig,
+            cards__galleryCard__enabled: newState,
+          });
+        }}
+      />
+    );
+  };
 
-  const SceneCardsEnabled = () => (
-    <BooleanSetting
-      checked={
-        props.pluginConfig.cards__sceneCard__enabled ??
+  const SceneCardsEnabled = () => {
+    const [checked, setChecked] = useState(
+      props.pluginConfig.cards__sceneCard__enabled ??
         DEFAULT.CARDS.SCENE_CARD.ENABLED
-      }
-      heading="Enable scene cards"
-      id="valkyr-ui-cards__sceneCard__enabled"
-      onChange={() =>
-        props.configUpdateHandler({
-          ...props.pluginConfig,
-          cards__sceneCard__enabled:
-            !props.pluginConfig.cards__sceneCard__enabled,
-        })
-      }
-    />
-  );
+    );
+    return (
+      <BooleanSetting
+        checked={checked}
+        heading="Enable scene cards"
+        id="valkyr-ui-cards__sceneCard__enabled"
+        onChange={() => {
+          const newState = !checked;
+          setChecked(newState);
+          props.configUpdateHandler({
+            ...props.pluginConfig,
+            cards__sceneCard__enabled: newState,
+          });
+        }}
+      />
+    );
+  };
 
-  const CardGridsEnabled = () => (
-    <BooleanSetting
-      checked={
-        props.pluginConfig.cards__cardGrid__enabled ??
-        DEFAULT.CARDS.CARD_GRID.ENABLED
-      }
-      heading="Enable card grids"
-      id="valkyr-ui-cards__cardGrid__enabled"
-      onChange={() =>
-        props.configUpdateHandler({
-          ...props.pluginConfig,
-          cards__cardGrid__enabled:
-            !props.pluginConfig.cards__cardGrid__enabled,
-        })
-      }
-      subHeading="Enables a smoother card grid component for enabled Valkyr UI card types. Closely resembles the native Stash card grids."
-    />
-  );
+  const CardGridsEnabled = () => {
+    const [checked, setChecked] = useState(
+      props.pluginConfig.cards__cardGrid__enabled ??
+        DEFAULT.CARDS.SCENE_CARD.ENABLED
+    );
+    return (
+      <BooleanSetting
+        checked={checked}
+        heading="Enable card grids"
+        id="valkyr-ui-cards__cardGrid__enabled"
+        onChange={() => {
+          const newState = !checked;
+          setChecked(newState);
+          props.configUpdateHandler({
+            ...props.pluginConfig,
+            cards__cardGrid__enabled: newState,
+          });
+        }}
+        subHeading="Enables a smoother card grid component for enabled Valkyr UI card types. Closely resembles the native Stash card grids."
+      />
+    );
+  };
 
   return (
     <SettingSection id="cards-enable" heading="Enable cards">
