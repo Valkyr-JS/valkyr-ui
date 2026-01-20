d67139b6a62d82f2532a8305c8e0bf1439ae32cb
ValkyrJS <valkyr.photos@gmail.com>
2026-01-20 23:10:25 +0000

    String setting component

---
+++ src/components/stash/Settings/Inputs/StringSetting.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Setting from "./Setting";
+import { Form } from "react-bootstrap";
+
+interface StringSettingProps extends ISetting {
+  onChange: (v: string) => void;
+  placeholder?: string;
+  value: string | undefined;
+}
+// This is a custom build component based on the NumberSetting component, rather
+// than directly copied from a Stash component.
+export const StringSetting: React.FC<StringSettingProps> = (props) => {
+  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) =>
+    props.onChange(e.target.value);
+
+  return (
+    <Setting {...props}>
+      <Form.Control
+        className="text-input"
+        id={props.id}
+        onChange={handleChange}
+        placeholder={props.placeholder}
+        type="text"
+        value={props.value}
+      />
+    </Setting>
+  );
+};
