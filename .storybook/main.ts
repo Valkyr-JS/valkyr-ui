634de604f1baae65ea56a46a78328ac500bc7f02
ValkyrJS <valkyr.photos@gmail.com>
2026-01-25 11:57:19 +0000

    Storybook upgrade to 10.2.0

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,9 +1,10 @@
+// This file has been automatically migrated to valid ESM format by Storybook.
 import type { StorybookConfig } from "@storybook/react-vite";
-import path from "path";
+import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = dirname(__filename);
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
