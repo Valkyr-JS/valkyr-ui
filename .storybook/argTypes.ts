b6e2ca283df38123ecf4254d14f3166920ee7c9e
ValkyrJS <valkyr.photos@gmail.com>
2026-01-25 00:26:50 +0000

    ratingArgType

--- .storybook/argTypes.ts
+++ .storybook/argTypes.ts
@@ -1,6 +1,6 @@
 import { ArgTypes } from "storybook/internal/csf";
 
-/** Common arg types used for data components  */
+/** Common arg types used for data components.  */
 export const dataComponentArgTypes: Partial<
   ArgTypes<DataComponentProps | DataComponentModalProps>
 > = {
@@ -12,5 +12,48 @@ export const dataComponentArgTypes: Partial<
   },
   userBreakpoint: {
     control: { type: "range", min: -1, max: 3 },
+  },
+};
+
+export const ratingSystemOptions = {
+  decimal: {
+    type: "decimal",
+  },
+  starsFull: {
+    type: "stars",
+    starPrecision: "full",
+  },
+  starsHalf: {
+    type: "stars",
+    starPrecision: "half",
+  },
+  starsQuarter: {
+    type: "stars",
+    starPrecision: "quarter",
+  },
+  starsTenth: {
+    type: "stars",
+    starPrecision: "tenth",
+  },
+};
+
+/** Common arg types used for ratings data. */
+export const ratingArgType: Partial<ArgTypes> = {
+  rating100: {
+    control: { type: "range", min: 0, max: 100 },
+  },
+  ratingSystem: {
+    options: Object.keys(ratingSystemOptions),
+    mapping: ratingSystemOptions,
+    control: {
+      type: "select",
+      labels: {
+        decimal: "Decimal",
+        starsFull: "Stars (full)",
+        starsHalf: "Stars (half)",
+        starsQuarter: "Stars (quarter)",
+        starsTenth: "Stars (tenth)",
+      },
+    },
   },
 };
