aad44074ec9af4a04ea4af85e17c5ee5737dd62e
ValkyrJS <valkyr.photos@gmail.com>
2026-01-25 00:01:10 +0000

    Scene card o count stories

--- src/components/cards/SceneCard/SceneCard.stories.tsx
+++ src/components/cards/SceneCard/SceneCard.stories.tsx
@@ -1,18 +1,12 @@
 import React from "react";
 import type { Meta, StoryObj } from "@storybook/react-vite";
 import { expect, within } from "storybook/test";
-import { DEFAULT } from "@/constants";
 import SceneCard from ".";
 
 // Mock data
 import filelessData from "../../../../mocks/scenes/filelessData.slim.json";
 import fullData from "../../../../mocks/scenes/fullData.slim.json";
 import minimalData from "../../../../mocks/scenes/minimalData.slim.json";
-
-const pluginConfig = {
-  cards__sceneCard__ratingIconBreakpoint:
-    DEFAULT.CARDS.SCENE_CARD.RATING_ICON_BREAKPOINT,
-};
 
 const meta = {
   title: "Components/Cards/Scene card",
@@ -28,7 +22,7 @@ const meta = {
     layout: "centered",
   },
   args: {
-    pluginConfig,
+    pluginConfig: {},
     zoomBreakpoint: 1,
   },
   argTypes: {
@@ -42,7 +36,7 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const FullData: Story = {
+export const FullDataDefaults: Story = {
   args: {
     scene: fullData as SlimSceneDataFragment,
   },
@@ -62,6 +56,40 @@ export const FullData: Story = {
     // Duration should render
     const duration = canvas.getByText("Duration: 37 minutes 55 seconds");
     await expect(duration).toBeInTheDocument();
+
+    // O count shount NOT render
+    const oCount = canvas.queryByText("O Count: 2");
+    await expect(oCount).toBeNull();
+  },
+};
+
+export const FullDataAllEnabled: Story = {
+  args: {
+    pluginConfig: {
+      cards__sceneCard__oCountBreakpoint: 0,
+    },
+    scene: fullData as SlimSceneDataFragment,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    // Date should render
+    const date = canvas.getByText("Date: 11 April 2016");
+    await expect(date).toBeInTheDocument();
+
+    // Details should render
+    const details = canvas.getByText(
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus lectus odio, fermentum sed egestas et, laoreet et enim. Aenean vulputate metus dolor, at placerat tortor porta non. Proin vel faucibus mauris. Mauris nec eleifend augue. In sed augue a felis aliquam gravida et aliquet risus. Nulla malesuada massa a nisi rutrum vestibulum. Suspendisse potenti. Donec laoreet tristique rhoncus. Nam porttitor mollis odio eu fermentum. Fusce magna mauris, scelerisque ac mollis eu, congue id sapien. Fusce at mauris at justo condimentum laoreet.",
+    );
+    await expect(details).toBeInTheDocument();
+
+    // Duration should render
+    const duration = canvas.getByText("Duration: 37 minutes 55 seconds");
+    await expect(duration).toBeInTheDocument();
+
+    // O count should render
+    const count = canvas.getByText("O Count: 2");
+    await expect(count).toBeInTheDocument();
   },
 };
 
