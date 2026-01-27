65ac42ac959c5470fa9c84ea278785e50af6c301
ValkyrJS <valkyr.photos@gmail.com>
2026-01-27 20:50:50 +0000

    Image collection icon config type and default

--- types/valkyr-ui.d.ts
+++ types/valkyr-ui.d.ts
@@ -68,6 +68,9 @@ interface ValkyrUiConfigMap {
   cards__galleryCard__enabled?: boolean;
   /** The zoom index at which to display the file size on gallery cards. */
   cards__galleryCard__fileSizeZoomIndex?: StashCardGridZoom;
+  /** The zoom index at which to display the image collection icon on gallery
+   * cards. */
+  cards__galleryCard__imageCollectionIconZoomIndex?: StashCardGridZoom;
   /** The zoom index at which to display the organized icon on gallery
    * cards. */
   cards__galleryCard__organizedZoomIndex?: StashCardGridZoom;
