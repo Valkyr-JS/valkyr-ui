e7231841c4a28334694c5016df2ae3c776f82faa
ValkyrJS <valkyr.photos@gmail.com>
2026-01-27 17:34:01 +0000

    ZipIcon component

---
+++ src/components/cards/data/ZipIcon/index.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { faFileZipper } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useIntl } from "react-intl";
+import { getRenderData } from "@/helpers";
+import "./ZipIcon.scss";
+
+interface ZipIconProps {
+  /** Whether the media is a zip file. */
+  isZip: boolean;
+}
+
+const ZipIcon: React.FC<
+  DataComponentProps<ZipIconProps> | DataComponentModalProps<ZipIconProps>
+> = (props) => {
+  const intl = useIntl();
+
+  const data =
+    props.context === "modal"
+      ? props.isZip
+      : getRenderData({
+          data: props.isZip,
+          zoomIndex: {
+            current: props.currentZoomIndex,
+            user: props.userZoomIndex,
+          },
+        });
+
+  if (!data) return null;
+
+  const componentClass = "vui-card-data__zip-icon";
+
+  return (
+    <span className={componentClass}>
+      <FontAwesomeIcon icon={faFileZipper} />
+      <span className="sr-only">ZIP file gallery</span>
+    </span>
+  );
+};
+
+export default ZipIcon;
