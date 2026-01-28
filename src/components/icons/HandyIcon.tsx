554750c2b90152578fc06c5d61631dd62982aca0
ValkyrJS <valkyr.photos@gmail.com>
2026-01-28 15:28:38 +0000

    Handy icon

---
+++ src/components/icons/HandyIcon.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const HandyIcon: React.FC = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    aria-hidden="true"
+    focusable="false"
+    width="1em"
+    height="1em"
+    style={{ transform: "rotate(360deg)" }}
+    viewBox="0 0 27 35"
+    preserveAspectRatio="xMidYMid meet"
+  >
+    <path
+      fill="currentColor"
+      d="M12.5827 6.68799V28.9448C12.5827 32.3673 9.75344 35.1652 6.33879 35.0078C3.15883 34.8503 0.65973 32.2279 0.649414 29.0429V6.78091C0.649414 3.55712 3.16398 0.849544 6.38005 0.723071C9.68123 0.601759 12.4563 3.18286 12.5775 6.48666C12.5775 6.55377 12.5801 6.62088 12.5827 6.68799Z"
+    ></path>
+    <path
+      fill="currentColor"
+      d="M25.9858 20.9563V34.4064H14.0552V20.9563C14.0552 17.6576 16.7271 14.9862 20.0205 14.9862C23.314 14.9862 25.9858 17.6602 25.9858 20.9563Z"
+    ></path>
+  </svg>
+);
+
+export default HandyIcon;
