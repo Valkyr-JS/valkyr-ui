228b3bcd3482f52e14337ec18a2c93d3b6d0940a
ValkyrJS <valkyr.photos@gmail.com>
2026-01-31 18:29:49 +0000

    Fix optionSorter

--- src/components/stash/Settings/Inputs/OrderSettingRenderField.tsx
+++ src/components/stash/Settings/Inputs/OrderSettingRenderField.tsx
@@ -16,7 +16,8 @@ interface OrderOption {
 }
 
 const optionSorter = (a: OrderOption, b: OrderOption) => {
-  if (!a.disabled && b.disabled) return -1;
+  if (a.disabled === b.disabled) return 0;
+  if (!a.disabled) return -1;
   if (!b.disabled) return 1;
   return 0;
 };
