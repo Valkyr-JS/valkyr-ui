097edded766b725b82bcc9d3037424f6efb0d369
ValkyrJS <valkyr.photos@gmail.com>
2026-02-10 13:45:23 +0000

    Refactor data rendering check

--- src/components/cards/layouts/CardModal/sections/CardModalFileInfoSection.tsx
+++ src/components/cards/layouts/CardModal/sections/CardModalFileInfoSection.tsx
@@ -28,6 +28,27 @@ const CardModalFileInfoSection: React.FC<CardModalFileInfoSectionProps> = (
   const componentClass = "vui-card-modal";
   const sectionClass = componentClass + "__file-info-section";
 
+  /* ----------------------------------------- Dimensions ----------------------------------------- */
+
+  const maybeRenderDimensions = (
+    file: GalleryFileDataFragment | VideoFileDataFragment,
+  ) => {
+    if (!("height" in file)) return null;
+
+    const dimensions = file.height + " x " + file.width;
+    const dimensionsSr = file.height + " by " + file.width;
+
+    return (
+      <tr>
+        <th>{intl.formatMessage({ id: "duration" })}</th>
+        <td>
+          <span aria-hidden>{dimensions}</span>
+          <span className="sr-only">{dimensionsSr}</span>
+        </td>
+      </tr>
+    );
+  };
+
   /* ------------------------------------------ Duration ------------------------------------------ */
 
   const maybeRenderDuration = (
@@ -51,18 +72,67 @@ const CardModalFileInfoSection: React.FC<CardModalFileInfoSectionProps> = (
     );
   };
 
+  /* ----------------------------------- File modification time ----------------------------------- */
+
+  const maybeRenderModTime = (
+    file: GalleryFileDataFragment | VideoFileDataFragment,
+  ) => {
+    return (
+      <tr>
+        <th>{intl.formatMessage({ id: "file_mod_time" })}</th>
+        <td>
+          <FormattedDate
+            value={file.mod_time}
+            dateStyle="short"
+            timeStyle="medium"
+            timeZone="utc"
+          />
+        </td>
+      </tr>
+    );
+  };
+
+  /* ------------------------------------------ File size ----------------------------------------- */
+
+  const maybeRenderFileSize = (
+    file: GalleryFileDataFragment | VideoFileDataFragment,
+  ) => {
+    const [fileSize, divisions] = roundBytes(file.size);
+
+    return (
+      <tr>
+        <th>{intl.formatMessage({ id: "filesize" })}</th>
+        <td>
+          <span aria-hidden>
+            {fileSize} {fileSizeUnits[divisions][0]}
+          </span>
+          <span className="sr-only">
+            {fileSize} {fileSizeUnits[divisions][1]}
+          </span>
+        </td>
+      </tr>
+    );
+  };
+
+  /* -------------------------------------------- Path -------------------------------------------- */
+
+  const maybeRenderPath = (
+    file: GalleryFileDataFragment | VideoFileDataFragment,
+  ) => {
+    return (
+      <tr>
+        <th>{intl.formatMessage({ id: "path" })}</th>
+        <td>{file.path}</td>
+      </tr>
+    );
+  };
+
   /* ------------------------------------------ Component ----------------------------------------- */
 
   return (
     <div className={sectionClass}>
       <ul>
         {props.files.map((f) => {
-          const [fileSize, divisions] = roundBytes(f.size);
-
-          const dimensions = "height" in f ? f.height + " x " + f.width : null;
-          const dimensionsSr =
-            "height" in f ? f.height + " by " + f.width : null;
-
           return (
             <li key={f.id}>
               <Table striped>
@@ -83,41 +153,10 @@ const CardModalFileInfoSection: React.FC<CardModalFileInfoSectionProps> = (
                       </tr>
                     );
                   })}
-                  <tr>
-                    <th>{intl.formatMessage({ id: "path" })}</th>
-                    <td>{f.path}</td>
-                  </tr>
-                  <tr>
-                    <th>{intl.formatMessage({ id: "filesize" })}</th>
-                    <td>
-                      <span aria-hidden>
-                        {fileSize} {fileSizeUnits[divisions][0]}
-                      </span>
-                      <span className="sr-only">
-                        {fileSize} {fileSizeUnits[divisions][1]}
-                      </span>
-                    </td>
-                  </tr>
-                  <tr>
-                    <th>{intl.formatMessage({ id: "file_mod_time" })}</th>
-                    <td>
-                      <FormattedDate
-                        value={f.mod_time}
-                        dateStyle="short"
-                        timeStyle="medium"
-                        timeZone="utc"
-                      />
-                    </td>
-                  </tr>
-                  {!!dimensions && (
-                    <tr>
-                      <th>{intl.formatMessage({ id: "dimensions" })}</th>
-                      <td>
-                        <span aria-hidden>{dimensions}</span>
-                        <span className="sr-only">{dimensionsSr}</span>
-                      </td>
-                    </tr>
-                  )}
+                  {maybeRenderPath(f)}
+                  {maybeRenderFileSize(f)}
+                  {maybeRenderModTime(f)}
+                  {maybeRenderDimensions(f)}
                   {maybeRenderDuration(f)}
                 </tbody>
               </Table>
