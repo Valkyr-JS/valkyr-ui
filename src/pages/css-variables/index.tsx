1074cffa77dcc73b8ecec6cc143db70340c820e3
ValkyrJS <valkyr.photos@gmail.com>
2026-02-10 21:36:23 +0000

    All data

--- src/pages/css-variables/index.tsx
+++ src/pages/css-variables/index.tsx
@@ -1,8 +1,249 @@
 import React from "react";
+import { SettingSection } from "@/components/stash/Settings/SettingSection";
+import { Table } from "react-bootstrap";
 
-const CssVariablesTab: React.FC = (props) => {
-  console.log(props);
-  return <div>CSS variables</div>;
+const CssVariablesTab: React.FC = () => {
+  const prefix = "valkyr-ui";
+
+  return (
+    <SettingSection id="general" heading="CSS variables">
+      <Table striped>
+        <thead>
+          <tr>
+            <th scope="col">CSS variable</th>
+            <th scope="col">Description</th>
+            <th scope="col">Default value</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">
+              <code>--{prefix}-body-color</code>
+            </th>
+            <td>The font color for most text.</td>
+            <td>
+              <code>#f5f8fa</code>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">
+              <code>--{prefix}-link-color</code>
+            </th>
+            <td>The font color for most links.</td>
+            <td>
+              <code>var(--{prefix}-body-color)</code>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">
+              <code>--{prefix}-title-color</code>
+            </th>
+            <td>The font color for most headings.</td>
+            <td>
+              <code>var(--{prefix}-body-color)</code>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">
+              <code>--{prefix}-card-bg-color</code>
+            </th>
+            <td>The background color for cards and card modals.</td>
+            <td>
+              <code>#30404d</code>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">
+              <code>--{prefix}-card-font-sm</code>
+            </th>
+            <td>The font size used in places with smaller text.</td>
+            <td>
+              <code>12px</code>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">
+              <code>--{prefix}-card-resolution-icon-bg-color</code>
+            </th>
+            <td>
+              The background color for the resolution icon on cards and card
+              modals.
+            </td>
+            <td>
+              <code>var(--{prefix}-body-color)</code>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">
+              <code>--{prefix}-card-resolution-icon-color</code>
+            </th>
+            <td>
+              The font color for the resolution icon on cards and card modals.
+            </td>
+            <td>
+              <code>var(--{prefix}-card-bg-color)</code>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">
+              <code>--{prefix}-card-title-color</code>
+            </th>
+            <td>
+              The font color used for titles and headings on cards and card
+              modals.
+            </td>
+            <td>
+              <code>var(--{prefix}-title-color)</code>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">
+              <code>--{prefix}-card-title-font-size</code>
+            </th>
+            <td>
+              The font size used for titles and headings on cards and card
+              modals.
+            </td>
+            <td>
+              <code>1.25rem</code>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">
+              <code>--{prefix}-card-top-line-font-size</code>
+            </th>
+            <td>
+              The font size of data on the top line above the title on cards and
+              card modals.
+            </td>
+            <td>
+              <code>var(--{prefix}-cards-font-sm)</code>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">
+              <code>--{prefix}-card-top-line-opacity</code>
+            </th>
+            <td>
+              The opacity of data on the top line above the title on cards and
+              card modals.
+            </td>
+            <td>
+              <code>0.8</code>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">
+              <code>--{prefix}-card-fileless-bg-color</code>
+            </th>
+            <td>
+              The background color for cards and card modals with no associated
+              file.
+            </td>
+            <td>
+              <code>#26333d</code>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">
+              <code>--{prefix}-card-modal-max-width</code>
+            </th>
+            <td>
+              The maximum width of card modals at browser window sizes of 992px
+              or larger.
+            </td>
+            <td>
+              <code>640px</code>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">
+              <code>--{prefix}-gender-color--female</code>
+            </th>
+            <td>
+              The font and background color used in various places to denote a
+              female performer.
+            </td>
+            <td>
+              <code>#f38cac</code>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">
+              <code>--{prefix}-gender-color--intersex</code>
+            </th>
+            <td>
+              The font and background color used in various places to denote an
+              intersex performer.
+            </td>
+            <td>
+              <code>#c8a2c8</code>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">
+              <code>--{prefix}-gender-color--male</code>
+            </th>
+            <td>
+              The font and background color used in various places to denote a
+              male performer.
+            </td>
+            <td>
+              <code>#89cff0</code>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">
+              <code>--{prefix}-gender-color--nonbinary</code>
+            </th>
+            <td>
+              The font and background color used in various places to denote a
+              nonbinary performer.
+            </td>
+            <td>
+              <code>#c8a2c8</code>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">
+              <code>--{prefix}-gender-color--transfemale</code>
+            </th>
+            <td>
+              The font and background color used in various places to denote a
+              transgender female performer.
+            </td>
+            <td>
+              <code>#c8a2c8</code>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">
+              <code>--{prefix}-gender-color--transmale</code>
+            </th>
+            <td>
+              The font and background color used in various places to denote a
+              transgender male performer.
+            </td>
+            <td>
+              <code>#c8a2c8</code>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">
+              <code>--{prefix}-gender-color--unknown</code>
+            </th>
+            <td>
+              The font and background color used in various places to denote a
+              performer with no gender data.
+            </td>
+            <td>
+              <code>#f5f8fa</code>
+            </td>
+          </tr>
+        </tbody>
+      </Table>
+    </SettingSection>
+  );
 };
 
 export default CssVariablesTab;
